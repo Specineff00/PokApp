c4952c8556d0812f71f46ce3783284a4b2bcdf61
Nikash Ramsorrun <specineff00@gmail.com>
2025-09-24 12:39:09 +0100

    Initial Commit

---
+++ PokApp/PokAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokAppApp.swift
+//  PokApp
+//
+//  Created by Nikash Yogesh Ramsorrun on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
